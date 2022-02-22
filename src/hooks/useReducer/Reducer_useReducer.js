4c532e5fdc66ac2adeb84ed70b0d479a01b16850
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-02-22 21:31:25 +0100

    first commit

---
+++ src/hooks/useReducer/Reducer_useReducer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ReducerTutorial() {
+    return (<div>
+
+    </div>);
+}
+
+export default ReducerTutorial;
---
+++ src/hooks/useState/State_useState.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function StateTutorial() {
+
+    const [value, setValue] = useState(0);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const handleCount = () => {
+        setValue(value + 1);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <u><h1>useState hook</h1></u>
+            <p>{value}</p>
+            <button onClick={handleCount}>COUNT</button>
+
+            <form>
+                <input type='text' onChange={(event) => handleChange(event)} label='input' />
+
+                <button type='submit' onSubmit={() => handleSubmit()}>Submit</button>
+            </form>
+
+            {inputValue}
+        </div>
+    );
+}
+
+export default StateTutorial;
