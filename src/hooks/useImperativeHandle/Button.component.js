4cf4d25ad62301fb154f8b4ef43258199deec777
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-02-25 22:38:39 +0100

    useImperativeHandle hook

--- src/hooks/useImperativeHandle/Button.component.js
+++ src/hooks/useImperativeHandle/Button.component.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
 
-function Button({ ...props }) {
+const Button = forwardRef((props, ref) => {
     const { text } = props;
 
     const [toggle, setToggle] = useState(true);
 
-    const handleClick = () => {
-        setToggle(!toggle)
-    }
+    useImperativeHandle(ref, () => ({
+        alterToggle() {
+            setToggle(!toggle);
+        },
+    }));
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', textAlign: 'center', margin: '1% auto' }}>
-            <button style={{ borderRadius: 5, padding: '2vh 2vw', margin: '2% auto', cursor: 'pointer' }}
-                onClick={handleClick}>
+            <button style={{ borderRadius: 5, padding: '2vh 2vw', margin: '2% auto', cursor: 'pointer' }}>
                 {text}
             </button>
             <span>{toggle && 'Toggle'}</span>
         </div>
-    )
-}
+    );
+})
 
 export default Button;
--- src/hooks/useImperativeHandle/ImperativeHandle_useImperativeHandle.js
+++ src/hooks/useImperativeHandle/ImperativeHandle_useImperativeHandle.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from './Button.component';
 
 function ImperativeHandleTutorial() {
+
+    const buttonRef = useRef(null);
+
     return (<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', flexDirection: 'column', marginTop: '1%' }}>
-        <button style={{ cursor: 'pointer', padding: '13px', borderRadius: 5 }}>Button From Parent</button>
-        <Button text='Click me' />
+        <button style={{ cursor: 'pointer', padding: '13px', borderRadius: 5 }} onClick={() => { buttonRef.current.alterToggle() }}>
+            Button From Parent
+        </button>
+        <Button text='Click me' ref={buttonRef} />
     </div>);
 }
 
