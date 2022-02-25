afa69f449af7235f09aba768c530e2f6233f30b1
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-02-25 17:05:10 +0100

    adding useState to RefTutorial to get dynamic textinput

--- src/hooks/useRef/Ref_useRef.js
+++ src/hooks/useRef/Ref_useRef.js
@@ -1,17 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 function RefTutorial() {
+
+    const [textInput, setTextInput] = useState('Alex');
 
     const inputRef = useRef(null);
 
     const handleClick = (event) => {
         event.preventDefault();
         inputRef.current.focus();
+        setTextInput(inputRef.current.value);
     }
 
     return (<div>
         <u><h1>useRef</h1></u>
-        <h3>Alex</h3>
+        <h3>{textInput}</h3>
         <form>
             <input type='text' placeholder='Ex...' ref={inputRef} />
             <button onClick={handleClick}>Change Name</button>
